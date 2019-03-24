97848558c0c2aff0e78d935f12e349762e13fe5a
pedro <pedroluismontesinosrivera@gmail.com>
2019-03-24 06:32:25 -0400

    added while loop, added colors and fixed small bug in the choose_magic method

--- main.py
+++ main.py
@@ -5,6 +5,18 @@ magic = [{"name": "Fire", "cost": 10, "dmg": 60},
          {"name": "Blizzard", "cost": 10, "dmg": 60}]
 
 player = Person(460, 65, 60, 34, magic)
-print(player.generate_spell_damage(0))
-print(player.generate_spell_damage(1))
+enemy = Person(1200, 65, 45, 25, magic)
 
+running = True
+i = 0
+
+print(bcolors.FAIL + bcolors.BOLD + "AN ENEMY ATTACKS" + bcolors.ENDC)
+while running:
+    print("======================")
+    player.choose_action()
+    choice = input("Choose action:")
+    index = int(choice) - 1
+
+    print("You chose", index)
+
+    running = False
